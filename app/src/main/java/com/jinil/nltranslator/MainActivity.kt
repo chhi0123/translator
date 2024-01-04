48b0bab648d7ac047b0ac549e22e4bdb8dedcd93
chlwlsdlf1 <chlwlsdlf1@gmail.com>
2024-01-04 22:20:46 +0900

    first launchable activity with compose!

--- app/src/main/java/com/jinil/nltranslator/MainActivity.kt
+++ app/src/main/java/com/jinil/nltranslator/MainActivity.kt
@@ -3,29 +3,50 @@ package com.jinil.nltranslator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.jinil.nltranslator.ui.theme.TranslatorTheme
+import androidx.compose.ui.tooling.preview.Preview
+import com.jinil.nltranslator.ui.theme.NlTranslatorTheme
 
 class MainActivity : ComponentActivity() {
-
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TranslatorTheme {
+            NlTranslatorTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier,
+                    modifier = Modifier.wrapContentSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     AlertDialog(
                         onDismissRequest = {},
-                    ) {}
+                        confirmButton = {},
+                        text = { Text(text = "first!") }
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NlTranslatorTheme {
+        Greeting("Android")
     }
 }
