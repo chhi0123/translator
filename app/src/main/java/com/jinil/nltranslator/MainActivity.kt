ea788a45e103d61c966b6cca6e64ea15c4e7b8f8
chlwlsdlf1 <chlwlsdlf1@gmail.com>
2024-01-07 18:37:51 +0900

    add backk button dispatcher callback

--- app/src/main/java/com/jinil/nltranslator/MainActivity.kt
+++ app/src/main/java/com/jinil/nltranslator/MainActivity.kt
@@ -1,52 +1,61 @@
 package com.jinil.nltranslator
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.addCallback
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.jinil.nltranslator.ui.theme.NlTranslatorTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NlTranslatorTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.wrapContentSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AlertDialog(
-                        onDismissRequest = {},
-                        confirmButton = {},
-                        text = { Text(text = "first!") }
-                    )
+                    AppContent()
                 }
             }
+        }
+        onBackPressedDispatcher.addCallback {
+            finish()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppContent() {
+    Column(
+        modifier = Modifier
+            .clip(shape = RoundedCornerShape(40.dp))
+            .padding(40.dp)
+    ) {
+        Text(text = "first!")
+        Spacer(modifier = Modifier
+            .height(12.dp)
+            .fillMaxWidth())
+        Button(onClick = { /*TODO*/ }) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NlTranslatorTheme {
-        Greeting("Android")
+        }
     }
 }
+
